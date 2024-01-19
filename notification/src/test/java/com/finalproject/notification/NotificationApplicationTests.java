f0fc105b53d89c9ac6d61657ab908746e9e74207
akmalreyhan17 <rayhanakmal17@gmail.com>
2024-01-19 14:25:11 +0700

    feat: add test for notification main app

---
+++ notification/src/test/java/com/finalproject/notification/NotificationApplicationTests.java
@@ -0,0 +1,14 @@
+package com.finalproject.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+		NotificationApplication.main(new String[] {});
+	}
+
+}
