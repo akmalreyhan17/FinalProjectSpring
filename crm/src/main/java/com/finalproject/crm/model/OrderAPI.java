947c29af3525d66e1e2c1cdd86daada43752eb01
akmalreyhan17 <rayhanakmal17@gmail.com>
2024-01-19 14:15:55 +0700

    feat: add main model OrderAPI for crm

---
+++ crm/src/main/java/com/finalproject/crm/model/OrderAPI.java
@@ -0,0 +1,81 @@
+package com.finalproject.crm.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class OrderAPI {
+    @NotBlank(message = "id mandatory")
+    String id;
+    
+    String productName;
+    Long quantity;
+    String status;
+    LocalDateTime createdDate;
+    LocalDateTime updatedDate;
+    String orderType;
+    String crudType;
+    Boolean newOrder = true;
+
+    public OrderAPI() {
+    }
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getProductName() {
+        return productName;
+    }
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public Long getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+    public LocalDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+    public void setUpdatedDate(LocalDateTime updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+    public String getCrudType() {
+        return crudType;
+    }
+    public void setCrudType(String crudType) {
+        this.crudType = crudType;
+    }
+
+    public String getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(String orderType) {
+        this.orderType = orderType;
+    }
+
+    public Boolean getNewOrder() {
+        return newOrder;
+    }
+
+    public void setNewOrder(Boolean newOrder) {
+        this.newOrder = newOrder;
+    }
+}
