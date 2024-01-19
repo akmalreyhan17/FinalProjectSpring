457d87238623f9b904e5fbea1c76ba2d8bfe9ad2
akmalreyhan17 <rayhanakmal17@gmail.com>
2024-01-19 14:14:48 +0700

    feat: add config jms for crm

---
+++ crm/src/main/java/com/finalproject/crm/config/JMSConfig.java
@@ -0,0 +1,59 @@
+package com.finalproject.crm.config;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import com.finalproject.crm.model.OrderAPI;
+
+import jakarta.jms.ConnectionFactory;
+
+@Configuration
+public class JMSConfig {
+    @Value("${spring.activemq.broker-url}")
+    private String brokerUrl;
+
+    @Bean
+    public JmsListenerContainerFactory<?> queueFactory(ConnectionFactory connectionFactory,
+            DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        factory.setPubSubDomain(true);
+
+        return factory;
+    }
+
+    @Bean
+    public MessageConverter jsonConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+
+        Map<String, Class<?>> typeIdMappings = new HashMap<String, Class<?>>();
+        typeIdMappings.put("JMS_TYPE", OrderAPI.class);
+
+        converter.setTypeIdMappings(typeIdMappings);
+
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+
+    @Bean
+    public ActiveMQConnectionFactory jmsConnectionFactory() {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL(brokerUrl);
+        connectionFactory.setTrustedPackages(List.of("com.phincon.akmal"));
+        return connectionFactory;
+    }
+}
