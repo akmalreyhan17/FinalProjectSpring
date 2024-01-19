472ca8fdbc44eedb3afcff16ce325bb79c0cae06
akmalreyhan17 <rayhanakmal17@gmail.com>
2024-01-19 14:19:51 +0700

    feat: add test for main crm app

---
+++ crm/src/test/java/com/finalproject/crm/CrmApplicationTests.java
@@ -0,0 +1,14 @@
+package com.finalproject.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+		CrmApplication.main(new String[] {});
+	}
+
+}
